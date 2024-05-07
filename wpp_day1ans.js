768c6913129ad00d9adf76f7b0c18ee56fe7b3c6
gladson65 <grauth65@gmail.com>
2024-05-07 13:41:02 +0530

    wpp_day1ans

---
+++ wpp_day1ans.js
@@ -0,0 +1,24 @@
+// Day
+const day = new Date().getDay();
+const dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+console.log(`Today is: ${dayName[day]}.`)
+
+
+// time
+let hours = new Date().getHours();
+let minutes = new Date().getMinutes();
+let seconds = new Date().getSeconds();
+
+// check AM or PM 
+let format = hours >= 12 ? 'PM' : 'AM';
+
+// Live Time
+hours = hours % 12;
+hours = hours ? hours : 12;
+// if minutes and seconds less than 0 then 0 at first place
+minutes = minutes < 10 ? '0' + minutes : minutes;
+seconds = seconds < 10 ? '0' + seconds : seconds;
+// display time
+console.log(`${hours}${format}:${minutes}:${seconds}`)
+
+
