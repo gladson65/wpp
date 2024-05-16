875fa6becad8c6a391190ea674c93f117aaa7d7d
gladson65 <grauth65@gmail.com>
2024-05-16 19:52:33 +0530

    update month index: starting from 1

--- wpp_4.js
+++ wpp_4.js
@@ -1,23 +1,31 @@
 // date object
 let date = new Date();
 
-// current date
-let today = new Date(`${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`);
+function nextChristmas() {
 
-// christmas date
-let christmasDate = new Date(`11/25/${date.getFullYear()}`);
+    // christmas date
+    let christmasDate = new Date(`12/25/${date.getFullYear()}`);
+
+    // if current date cross 25Dec then add 1 year for next christmas.
+    if (today > christmasDate) {
+        christmasDate = new Date(`12/25/${date.getFullYear() + 1}`);
+    }
 
 
-if (today > christmasDate) {
-    christmasDate = new Date(`11/25/${date.getFullYear() + 1}`);
+    // calculate the time difference in miliseconds
+    let differenceTime = christmasDate.getTime() - today.getTime();
+
+    // calculate the no of days between two dates
+    let diffInDays = Math.ceil(differenceTime / (1000 * 3600 * 24));
+
+    console.log(`Next Christmas in ${diffInDays} days`);
+    
 }
 
 
 
-// calculate the time difference in miliSeconds
-let differenceTime = christmasDate.getTime() - today.getTime();
+// current date
+let today = new Date(`${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`);
 
-// calculate the no of days between two dates
-let diffInDays = Math.ceil(differenceTime / (1000 * 3600 * 24));
 
-console.log(`Next Christmas in ${diffInDays} days`);
+nextChristmas(today)
